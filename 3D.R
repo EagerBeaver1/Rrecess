58d6aaa33b8f8d8ef6e59d2483a842c5ff861e06
Eager Beaver <51525676+EagerBeaver1@users.noreply.github.com>
2019-06-13 06:06:39 -0700

    Update 3D.R

--- 3D.R
+++ 3D.R
@@ -6,3 +6,4 @@ x=round(data1$Visits/sum(data1$Visits)*100)
 label=paste(label,x)
 label=paste(label,"%",sep = "")
 pie3D(data1$Visits,labels=label,explode=0.05,col=c("wheat","blue","red","yellow","pink","green"),main=" FOOD HYGIENE 3D PIE CHART")
+#am adding changes
